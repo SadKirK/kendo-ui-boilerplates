e0818298ce60d476d92282c67bd651f89a7c6c0d
Cody Lindley <contact@codylindley.com>
2016-07-11 16:44:48 -0600

    contine to refine react wrapper

--- core-jquery-react-webpack-es6/src/app.js
+++ core-jquery-react-webpack-es6/src/app.js
@@ -21,7 +21,22 @@ var data = [{
 import KendoDropDownList from './k-ui-dropdown.js';
 
 var App = function(){
-    return (<div><KendoDropDownList options={{dataSource:data, dataTextField: "text", dataValueField: "value"}} /></div>);
+    return (
+		<div>
+			<KendoDropDownList KendoOptions={{dataSource:data, dataTextField: "text", dataValueField: "value"}}><input className="kendoDropDownList" /></KendoDropDownList>
+
+			<KendoDropDownList>
+				<select>
+	                <option>S - 6 3/4"</option>
+	                <option>M - 7 1/4"</option>
+	                <option>L - 7 1/8"</option>
+	                <option>XL - 7 5/8"</option>
+		        </select>
+			</KendoDropDownList>
+
+			<KendoDropDownList KendoOptions={{dataSource:data, dataTextField: "text", dataValueField: "value"}} />
+		</div>
+	);
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- core-jquery-react-webpack-es6/src/k-ui-dropdown.js
+++ core-jquery-react-webpack-es6/src/k-ui-dropdown.js
@@ -5,14 +5,24 @@ import ReactDOM from 'react-dom';
 
 const KendoDropDownList = React.createClass({
 	componentDidMount: function() {
-		//const { text, className, ...options } = this.props;
-		$(ReactDOM.findDOMNode(this)).kendoDropDownList(this.props.options);
+
+		//reference to the the node passed in, or a div. And, ref to node wrapped with jQuery
+		var elementNode;
+		var $elementNode;
+		this.elementNode = elementNode = ReactDOM.findDOMNode(this);
+		this.$elementNode = $elementNode = $(this.elementNode);
+
+		//instantiate the Kendo UI widget
+		$elementNode.kendoDropDownList(this.props.KendoOptions);
+
+		//setup static references on Widget
+		this.span = $elementNode.getKendoDropDownList().span
 	},
 	componentWillUnmount: function() {
 		$(ReactDOM.findDOMNode(this)).getKendoDropDownList().destroy();
 	},
 	render: function() {
-		return <input className="kendoDropDownList" /> ;
+		return this.props.children ? this.props.children : <div/>;
 	}
 });
 
